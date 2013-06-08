ec874fb58d21be163abed9d302eb46c67047e00b
Thomas Levine <occurrence@thomaslevine.com>
2013-06-08 16:58:55 -0700

    get form data in javascript

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 window.onload = function() {
   Parse.initialize("1znInkUwxEU1tSd7Ee7SX9WL9c3whQXh4esOLREB", "eNGcDrnLPePX7kQcjd3L5HsHWdQXX9HzHskP18nG");
-  document.getElementById('broadcast').addEventListener('submit', function(e) {
+  $('#broadcast').submit(function(e) {
     e.preventDefault()
-    alert(3)
+    alert($('#broadcast').serialize())
     return false
-  }, false)
+  })
 }
