771f00fa20378df98c8bd7117efc2b1e129e9d28
Thomas Levine <occurrence@thomaslevine.com>
2013-06-08 16:54:24 -0700

    listen for form submission

--- public/app.js
+++ public/app.js
@@ -1,2 +1,8 @@
-Parse.initialize("1znInkUwxEU1tSd7Ee7SX9WL9c3whQXh4esOLREB", "eNGcDrnLPePX7kQcjd3L5HsHWdQXX9HzHskP18nG");
-
+window.onload = function() {
+  Parse.initialize("1znInkUwxEU1tSd7Ee7SX9WL9c3whQXh4esOLREB", "eNGcDrnLPePX7kQcjd3L5HsHWdQXX9HzHskP18nG");
+  document.getElementById('broadcast').addEventListener('submit', function(e) {
+    e.preventDefault()
+    alert(3)
+    return false
+  }, false)
+}
