9d1000919318acf448946db2f014ffec0185b1e9
Thomas Levine <occurrence@thomaslevine.com>
2013-06-08 17:18:12 -0700

    yay data gets sent to parse

--- public/app.js
+++ public/app.js
@@ -2,12 +2,15 @@ window.onload = function() {
   Parse.initialize("1znInkUwxEU1tSd7Ee7SX9WL9c3whQXh4esOLREB", "eNGcDrnLPePX7kQcjd3L5HsHWdQXX9HzHskP18nG");
   $('#broadcast').submit(function(e) {
     e.preventDefault()
-    var formdata = Parse._.reduce($('#broadcast').serializeArray(), function(input, output) {
+    var data = Parse._.reduce($('#broadcast').serializeArray(), function(input, output) {
       input[output.name] = output.value
       return input
     }, {})
+    data.date = new Date(data.date)
+    window.data = data
+
     var incident = new Parse.Object('Incident')
-    incident.save(formdata)
+    incident.save(data)
     return false
   })
 }
