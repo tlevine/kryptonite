5e583c95454c53bc385c035de51679a7a761d976
Thomas Levine <occurrence@thomaslevine.com>
2013-06-08 17:15:22 -0700

    structure form data

--- public/app.js
+++ public/app.js
@@ -2,7 +2,12 @@ window.onload = function() {
   Parse.initialize("1znInkUwxEU1tSd7Ee7SX9WL9c3whQXh4esOLREB", "eNGcDrnLPePX7kQcjd3L5HsHWdQXX9HzHskP18nG");
   $('#broadcast').submit(function(e) {
     e.preventDefault()
-    alert($('#broadcast').serialize())
+    var formdata = Parse._.reduce($('#broadcast').serializeArray(), function(input, output) {
+      input[output.name] = output.value
+      return input
+    }, {})
+    var incident = new Parse.Object('Incident')
+    incident.save(formdata)
     return false
   })
 }
