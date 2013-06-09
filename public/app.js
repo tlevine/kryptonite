e4bcfb9c938bea32fc970b8a802c25fd3aa279ec
Thomas Levine <occurrence@thomaslevine.com>
2013-06-08 17:54:44 -0700

    extend Parse.Object

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   Parse.initialize("1znInkUwxEU1tSd7Ee7SX9WL9c3whQXh4esOLREB", "eNGcDrnLPePX7kQcjd3L5HsHWdQXX9HzHskP18nG");
+  var Incident = Parse.Object.extend('Incident')
   $('#broadcast').submit(function(e) {
     e.preventDefault()
     var data = Parse._.reduce($('#broadcast').serializeArray(), function(input, output) {
@@ -13,7 +14,7 @@ window.onload = function() {
     delete data.hour
 
     // Save it to parse
-    var incident = new Parse.Object('Incident')
+    var incident = new Incident
     incident.save(data)
 
     // For debugging
