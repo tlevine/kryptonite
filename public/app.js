1891d8d65dca1720feec5d38a5abc820184afa11
Thomas Levine <occurrence@thomaslevine.com>
2013-06-08 18:34:01 -0700

    look up times

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
   // Defaults
   var d = new Date()
-  $('input[type=date]').attr('value', (d.getFullYear() + '-0' + d.getMonth() + '-0' + d.getDay()).replace('00','0'))
+  $('input[type=date]').attr('value', (d.getFullYear() + '-0' + (d.getMonth() + 1) + '-0' + d.getDate()).replace('00','0'))
 
   $('#broadcast').submit(function(e) {
     e.preventDefault()
@@ -19,20 +19,31 @@ window.onload = function() {
     data.date.setHours(data.date.getHours() + (1 * data.hour))
     delete data.hour
 
-    // Look up nearby location readings.
+    // Look up nearby readings in location
     var incidentLocation = new Parse.GeoPoint(37.822802, -122.373962)
     var query = new Parse.Query(Location)
-    query.near('location', incidentLocation)
-      .lessThan('date', new Date(data.date + 3600))
-      .greaterThan('date', new Date(data.date - 3600))
+
+    // and in time
+    var startDate = new Date()
+    var endDate = new Date()
+    startDate.setTime(data.date.getTime())
+    endDate.setTime(data.date.getTime())
+    startDate.setHours(startDate.getHours() - 6)
+    endDate.setHours(endDate.getHours() + 6)
+
+    // Run the query.
+    query
+      .near('location', incidentLocation)
+      .lessThanOrEqualTo('date', endDate)
+      .greaterThanOrEqualTo('date', startDate)
       .select('udid')
       .find().then(function(locations){
-        window.l = locations
+        console.log(locations)
        })
 
     // Save it to parse
     var incident = new Incident
-  //incident.save(data)
+    incident.save(data)
 
     // For debugging
     window.data = data
